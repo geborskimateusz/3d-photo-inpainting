2004fc715c198cf1067333e508aa7d974e4ae95a
geborskimateusz <mateuszgeborski@gmail.com>
2020-05-23 23:49:00 +0200

    working on file download

--- app.py
+++ app.py
@@ -1,39 +1,38 @@
 import os
 from flask import Flask, render_template, request, redirect, send_file, url_for
-from main import render
+from main import render_mp4
 import boto3
 import botocore
 from tempfile import TemporaryDirectory
 import os, os.path
 from io import BytesIO
 from PIL import Image
+import uuid
+from flask import send_file, send_from_directory, safe_join, abort
 
 BUCKET_NAME = "insta3d"
 
 app = Flask(__name__)
 
 
-@app.route("/get/<user_id>/<image_id>", methods=["GET"])
-def upload(userid: str, image_id: str):
-    if request.method == "GET":
-        bucket_file_name = userid + "/" + image_id + "/image/penguinuhh-c8HSJgY2Do4-unsplash.jpg"
+@app.route("/render/<filer_name>", methods=["POST"])
+def render(filer_name: str):
+    if request.method == "POST":
+        f = request.files["file"]
 
-        input_dir = download_s3_file(bucket_file_name)
+        with TemporaryDirectory() as tempdir:
+            in_dir = TemporaryDirectory(dir=tempdir)
+            out_dir = TemporaryDirectory(dir=tempdir)
 
-        render(input_dir)
+            image = Image.open(BytesIO(f.read()))
+
+            image.save(in_dir.name + "/image.jpg", "JPEG")
+
+            render_mp4(in_dir.name, out_dir.name)
+
+            filename = filer_name + ".mp4"
+            return send_from_directory(out_dir.name, filename=filename, as_attachment=True)
 
 
 if __name__ == "__main__":
     app.run()
-
-
-def download_s3_file(bucket_file_name: str):
-    s3 = boto3.client("s3")
-    with TemporaryDirectory() as tempdir:
-        # tempdir = tempfile.mkdtemp(prefix="myapplication-", suffix="image")
-        file_byte_string = s3.get_object(Bucket=BUCKET_NAME, Key=bucket_file_name)["Body"].read()
-        image = Image.open(BytesIO(file_byte_string))
-
-        image.save(tempdir + "/image.jpg", "JPEG")
-
-        return tempdir
